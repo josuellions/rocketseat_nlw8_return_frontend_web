1c742287f1e2f47275addc72bd6f34c8baa5ec27
josuel A. Lopes <josuellions@gmail.com>
2022-05-07 13:33:38 -0300

    projeto completo - Add mensagem na home

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,5 +2,14 @@ import { Widget } from "./components/Widget"
 
 export function App() {
 
-  return <Widget/>
+  return( 
+  <>
+    <div className="container">
+      <h1 className="title">Deixe seu Feedback</h1>
+      <p className="message">Sobre a funcionalidade desenvolvida para capturar print da tela screenshot, escrever mensagem e enviar.</p>
+      <p className="menssage-link">Click no icone abaixo</p>
+    </div>
+    <Widget/>
+  </>
+  )
 }
